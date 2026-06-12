b69015bdee249885533894a195168c7fa981aba8
s0188279 <anya_atamas@mail.ru>
2026-06-12 11:33:56 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 int main() {
-    // Запрос имени пользователя у оператора
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name; // Считывание введенного имени
+    // Ask for username
+std::string name;
+std::cout << "Enter your name: ";
+std::cin >> name; // Reading name
 
     // Вывод персонализированного приветствия в консоль
     std::cout << "Hello world from @" << name << std::endl;
