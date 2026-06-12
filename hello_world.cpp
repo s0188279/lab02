9cb616eb338bbae8e5d5559d17b4e5c0a81cdec5
s0188279 <anya_atamas@mail.ru>
2026-06-12 11:27:04 +0300

    изменен стиль форматирования кода под Mozilla stylee

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,15 @@
 ﻿#include <iostream>
 #include <string>
 
-int main() {
+
+
+int main()
+{
     // Ask for username
-std::string name;
-std::cout << "Enter your name: ";
-std::cin >> name; // Reading name
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name; // Reading name
+
 
     // Вывод персонализированного приветствия в консоль
     std::cout << "Hello world from @" << name << std::endl;
